b15311a7206de5ff77d56d2c34c005116c8f0529
Oscar Quiroz <oscar.quiroz@delfosti.com>
2024-03-29 17:33:25 -0500

    First commit

---
+++ src/main/java/oscorp/msvcusers/MsvcUsersApplication.java
@@ -0,0 +1,13 @@
+package oscorp.msvcusers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsvcUsersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsvcUsersApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oscorp/msvcusers/MsvcUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package oscorp.msvcusers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsvcUsersApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
